c897b284f056326f73039cea7ea641613b9ef41d
Laurence @ldarnbr <ldarnbr@gmail.com>
2026-04-21 22:01:26 +0100

    feat: first draft at crawler.py to obtain html content

---
+++ src/crawler.py
@@ -0,0 +1,58 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+"""
+Using a class means that the crawler can manage its state, keeping track
+of the various pages visited/to visit. Normal Python functions would forget
+this context without messy passing of lists back and forth every time functions
+are called.
+"""
+class Crawler:
+  def __init__(self):
+    self.new_pages = []
+    # Need to store visited pages to prevent pages that link back
+    # to eachother causing an infinite loop.
+    self.visited_pages = set()
+
+  def fetch_page(self, url):
+    """
+    Requests library gets the HTML content and BeautifulSoup returns this as
+    a BS object.
+    """
+    try:
+      response = requests.get(url)
+      if response.status_code == 200:
+        return BeautifulSoup(response.text, 'html.parser')
+      else:
+        print(f"{response.status_code}: Error when fetching {url}")
+    except Exception as e:
+      print(f"Error occured fetching {url}: {e}")
+      return None
+  
+  def crawl(self, first_url):
+    """
+    """
+    self.new_pages.append(first_url)
+
+    # Continue until all new pages are explored
+    # Note: Perhaps restrict this to a specific number of travels?
+    while self.new_pages:
+      # FIFO ordering of exploration - also pops url off the list
+      url = self.to_visit.pop(0)
+
+      # skip past visited pages
+      if url in self.visited_pages:
+        continue
+
+      print(f"Visiting: {url}")
+      content = self.fetch_page(url)
+
+      if content:
+        # Extract words
+        # Extract new pages
+        self.visited_pages.add(url)
+        print("Waiting to grab another page")
+        time.sleep(6)
+
+
